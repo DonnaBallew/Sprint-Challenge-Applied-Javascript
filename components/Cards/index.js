71a4369f55ab1c1b39d5c8add2fdb2bd110bde72
Donna Ballew <ballewdd@gmail.com>
2020-05-15 20:11:52 -0700

    created cards components; MVP met

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,62 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+function myCard(info) {
+  const newCard = document.createElement("div");
+  const newHeadline = document.createElement("div");
+  const newAuthor = document.createElement("div");
+  const newImageContainer = document.createElement("div");
+  const newImg = document.createElement("img");
+  const authorName = document.createElement("span");
+
+  // classes
+  newCard.classList.add("card");
+  newHeadline.classList.add("headline");
+  newAuthor.classList.add("author");
+  newImageContainer.classList.add("img-container");
+
+  //structure
+  newCard.appendChild(newHeadline);
+  newCard.appendChild(newAuthor);
+  newAuthor.appendChild(newImageContainer);
+  newImageContainer.appendChild(newImg);
+  newAuthor.appendChild(authorName);
+
+  //content
+  newHeadline.textContent = info.headline;
+  newImg.src = info.authorPhoto;
+  authorName.textContent = info.authorName;
+  // console.log(newCard);
+
+  return newCard;
+}
+
+const cardContainer = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then((response) => {
+    const article = response.data.articles;
+    console.log("article", article);
+
+    article.javascript.forEach((e) => {
+      cardContainer.appendChild(myCard(e));
+    });
+    article.bootstrap.forEach((e) => {
+      cardContainer.appendChild(myCard(e));
+    });
+    article.technology.forEach((e) => {
+      cardContainer.appendChild(myCard(e));
+    });
+    article.jquery.forEach((e) => {
+      cardContainer.appendChild(myCard(e));
+    });
+    article.node.forEach((e) => {
+      cardContainer.appendChild(myCard(e));
+    });
+  })
+
+  .catch((error) => {
+    console.log("Oops!  We can't find the data", error);
+  });
